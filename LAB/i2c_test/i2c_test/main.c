6177d0a9514e38c32009abaf2cade1be99e12312
Nevil K <nevil@anoralabs.com>
2024-01-10 10:14:46 +0530

    file added

---
+++ LAB/i2c_test/i2c_test/main.c
@@ -0,0 +1,18 @@
+/*
+ * i2c_test.c
+ *
+ * Created: 10-01-2024 09:43:48
+ * Author : nevil
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
